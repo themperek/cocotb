4c6f4401753bbc7778fa9ff0b93d71021e7ae7a2
Tomasz Hemperek <themperek@users.noreply.github.com>
2020-01-19 22:13:47 +0100

    Update build_libs.py

--- cocotb/build_libs.py
+++ cocotb/build_libs.py
@@ -1,21 +1,46 @@
-#!/bin/env python3
+#!/usr/bin/env python
+
+# Copyright (c) 2013 Potential Ventures Ltd
+# Copyright (c) 2013 SolarFlare Communications Inc
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of Potential Ventures Ltd,
+#       SolarFlare Communications Inc nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL POTENTIAL VENTURES LTD BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import os
 import sys
 import sysconfig
-import cocotb
 import errno
 import distutils
 import shutil
 import logging
-import distutils.log
+import pytest
 
 from setuptools import Extension
 from setuptools.dist import Distribution
 from distutils.spawn import find_executable
 from setuptools.command.build_ext import build_ext as _build_ext
 
-logger = logging.getLogger(__name__)
 
 # Needed for Windows to not assume python module (generate interface in def file)
 class build_ext(_build_ext):
@@ -212,6 +237,8 @@ def build_vhpi_lib(
 
 def build(build_dir="cocotb_build"):
 
+    logger = logging.getLogger(__name__)
+
     distutils.log.set_verbosity(0)  # Disable logging comiliation commands in disutils
     # distutils.log.set_verbosity(distutils.log.DEBUG) # Set DEBUG level
 
@@ -223,7 +250,7 @@ def build(build_dir="cocotb_build"):
     if sys.platform == "darwin":
         cfg_vars["LDSHARED"] = cfg_vars["LDSHARED"].replace("-bundle", "-dynamiclib")
 
-    share_dir = os.path.join(os.path.dirname(cocotb.__file__), "share")
+    share_dir = os.path.join(os.path.dirname(__file__), "share")
     share_lib_dir = os.path.join(share_dir, "lib")
     build_dir = os.path.abspath(build_dir)
     include_dir = os.path.join(share_dir, "include")
