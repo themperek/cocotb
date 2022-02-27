0b3c009dfe3fad62c8e311a083f6c430eac60b48
github-action-benchmark <github@users.noreply.github.com>
2022-02-27 12:54:11 +0000

    add Benchmark (pytest) benchmark result for 63a8d297b5ba8c81b5f011a9247c491c1065f03b

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1645966451412,
+  "repoUrl": "https://github.com/themperek/cocotb",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "themperek@users.noreply.github.com",
+            "name": "Tomasz Hemperek",
+            "username": "themperek"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "63a8d297b5ba8c81b5f011a9247c491c1065f03b",
+          "message": "Merge pull request #4 from themperek/benchmark\n\nAdd performance benchmark",
+          "timestamp": "2022-02-27T13:50:23+01:00",
+          "tree_id": "dc65d32d6a6bbf3e9047ff01ebb6d6d9be009383",
+          "url": "https://github.com/themperek/cocotb/commit/63a8d297b5ba8c81b5f011a9247c491c1065f03b"
+        },
+        "date": 1645966450598,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "::test_matrix_multiplier_icarus",
+            "value": 0.13703034267421754,
+            "unit": "iter/sec",
+            "range": "stddev: 0.03195543811421591",
+            "extra": "mean: 7.297653793200004 sec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
